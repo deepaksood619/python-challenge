662f2051dfaede4db66ea5d9cdeca1f3580a2c2f
Deepak Sood <deepaksood619@gmail.com>
2017-08-09 14:08:47 +0530

    Solution 1

---
+++ 1.py
@@ -0,0 +1,12 @@
+# Question - http://www.pythonchallenge.com/pc/def/map.html
+# Thought Process -
+#   Its a caesar's cipher with ROT2 as the image shows the
+#   alphabet shifting
+
+# ROT2 Shift answer - i hope you didnt translate it by hand. thats what computers are for. doing it in by hand is inefficient and that's why this text is so long. using string.maketrans() is recommended. now apply on the url.
+
+# ROT2 on map - ocr
+
+# used https://planetcalc.com/1434/ for directly calculating the ROT
+
+# solution - http://www.pythonchallenge.com/pc/def/ocr.html
