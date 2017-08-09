70821aac483d5a6dd4b1c53c38c8fc15ed8795e8
Deepak Sood <deepaksood619@gmail.com>
2017-08-09 13:31:05 +0530

    Solution 0

---
+++ 0.py
@@ -0,0 +1,6 @@
+# Question - http://www.pythonchallenge.com/pc/def/0.html
+# Thought process -
+#   Image shows 2^38, and hint is try to change the url address
+#   2^38 - 274877906944
+# Solution - http://www.pythonchallenge.com/pc/def/274877906944.html
+# Redirect - http://www.pythonchallenge.com/pc/def/map.html
